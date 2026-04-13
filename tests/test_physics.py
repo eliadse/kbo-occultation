ebe9f86e738e1282de3d1a361f9c5d9103267245
Elia do Souto <eliadse@gmail.com>
2026-04-13 17:58:55 +0200

    Deleted test

--- tests/test_physics.py
+++
@@ -1,7 +0,0 @@
-def test_planck_positive():
-    import numpy as np
-    from kbo_occultation.physics import planck_photon
-    
-    lam = np.array([500e-9])
-    assert planck_photon(lam, 5000) > 0
-
