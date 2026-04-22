2e253cd2a2dc31d09657320c1c9a2106c6191fcc
Elia do Souto <eliadse@gmail.com>
2026-04-22 15:21:38 +0200

    Added filter comparison

--- tests/test_impact_2D.py
+++ tests/test_impact_2D.py
@@ -12,27 +12,29 @@ from kbo_occultation import (
 
 grid = GridConfig(5000, 500)
 num = NumericalConfig()
-star = StarConfig(temperature_K=20000, angular_radius_mas=0.027)
-kbo = KBOConfig(radius_m=500, distance_au=40.0)
+star = StarConfig(temperature_K=30000, angular_radius_mas=0.03)
+kbo = KBOConfig(radius_m=50, distance_au=40.0)
 
 # Simulate mono point
 band = BandpassConfig(400, 430, 5)
 x, Imono = simulate_poly_point(kbo, star, band, grid, num)
 
 # Simulate poly point
-band = BandpassConfig(300, 500, 25)
+band = BandpassConfig(100, 800, 25)
 #x, Ipoly = simulate_poly_point(kbo, star, band, grid, num)
 
 # Simulate poly star, no offset
 x, Istar = compute_lightcurve(kbo, star, band, grid, num)
+x, Istar_SII = compute_lightcurve(kbo, star, band, grid, num, True)
 
-kbo = KBOConfig(radius_m=500, distance_au=40.0, impact_parameter_m = 100)
-x, Ioff = compute_lightcurve(kbo, star, band, grid, num)
+#kbo = KBOConfig(radius_m=500, distance_au=40.0, impact_parameter_m = 100)
+#x, Ioff = compute_lightcurve(kbo, star, band, grid, num)
 
 plt.plot(x, Imono, label='Monochromatic point source')
 #plt.plot(x, Ipoly, label='Polychromatic point source')
 plt.plot(x, Istar, label=f'{star.angular_radius_mas}mas radius star')
-plt.plot(x, Ioff, label=f'{star.angular_radius_mas}mas radius star, {kbo.impact_parameter_m}m offset')
+plt.plot(x, Istar_SII, label=f'{star.angular_radius_mas}mas radius star SII')
+#plt.plot(x, Ioff, label=f'{star.angular_radius_mas}mas radius star, {kbo.impact_parameter_m}m offset')
 plt.grid()
 plt.legend(loc="upper left")
 plt.xlabel("Position (m)")
